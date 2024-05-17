73244f29dcd8edbe3306d1ebce209d5bf6fe66c0
KTG5672 <zzang5992@gmail.com>
2024-05-17 23:00:38 +0900

    init

---
+++ src/main/java/com/cch/codechallengehub/CodeChallengeHubApplication.java
@@ -0,0 +1,13 @@
+package com.cch.codechallengehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cch/codechallengehub/CodeChallengeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cch.codechallengehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
