f3a3572ec203ad4fea836fab1693aaacb14c2d2e
haeunchoi-dev <haeunchoi.dev@gmail.com>
2024-09-09 23:54:43 +0900

    Refactor: CustomUserDetailsService for improved null handling and code simplification

--- src/main/java/com/cch/codechallengehub/service/CustomUserDetailsService.java
+++ src/main/java/com/cch/codechallengehub/service/CustomUserDetailsService.java
@@ -3,27 +3,27 @@ package com.cch.codechallengehub.service;
 import com.cch.codechallengehub.domain.User;
 import com.cch.codechallengehub.dto.CustomUserDetails;
 import com.cch.codechallengehub.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService  implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        User userData = userRepository.findByEmail(username);
+        Optional<User> userData = userRepository.findByEmail(username);
 
-        if (userData != null) {
-            return new CustomUserDetails(userData);
+        if (userData.isPresent()) {
+            return new CustomUserDetails(userData.get());
         }
 
         return null;
