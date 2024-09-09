025a864a471a32a8f97629aa44644ef80cff66ad
haeunchoi-dev <haeunchoi.dev@gmail.com>
2024-09-10 00:10:03 +0900

    Refactor: UserRepository
    - Changed ID type from Integer to String to match User entity.
    - Updated findByEmail method to return Optional<User> for better null safety.

--- src/main/java/com/cch/codechallengehub/repository/UserRepository.java
+++ src/main/java/com/cch/codechallengehub/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.cch.codechallengehub.repository;
 import com.cch.codechallengehub.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByEmail(String email);
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
