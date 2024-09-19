9b4734c632f263aeece0b91a5991d8feb7f07124
KTG5672 <zzang5992@gmail.com>
2024-09-19 22:39:05 +0900

    feat: 테스트 자동화 Action 추가(action-junit-report) 실패 테스트

---
+++ src/test/java/com/cch/codechallengehub/ci/CiTest.java
@@ -0,0 +1,17 @@
+package com.cch.codechallengehub.ci;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class CiTest {
+
+	@Test
+	void Test_fail() {
+	    // given
+		// when
+		// then
+		fail();
+	}
+
+}
