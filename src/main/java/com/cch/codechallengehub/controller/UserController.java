88b34dc08e839f1b12d794b557e95b20dde5cc47
haeunchoi-dev <haeunchoi.dev@gmail.com>
2024-10-05 11:42:18 +0900

    refactor: Remove null check for userDetails in UserController

--- src/main/java/com/cch/codechallengehub/controller/UserController.java
+++ src/main/java/com/cch/codechallengehub/controller/UserController.java
@@ -20,27 +20,18 @@ public class UserController {
 
     @GetMapping("/profile")
     public ResponseEntity<ProfileDto> getProfile(@AuthenticationPrincipal UserDetails userDetails) {
-        if (userDetails == null) {
-            throw new AuthException("No user authenticated");
-        }
         ProfileDto result = userService.getProfile(userDetails.getUsername());
         return ResponseEntity.ok(result);
     }
 
     @PostMapping("/profile")
     public ResponseEntity<Void> setProfile(@AuthenticationPrincipal UserDetails userDetails, @Valid @RequestBody ProfileDto profileDto) {
-        if (userDetails == null) {
-            throw new AuthException("No user authenticated");
-        }
         userService.setProfile(userDetails.getUsername(), profileDto);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/password")
     public ResponseEntity<Void> changePassword(@AuthenticationPrincipal UserDetails userDetails, @Valid @RequestBody PasswordDto passwordDto) {
-        if (userDetails == null) {
-            throw new AuthException("No user authenticated");
-        }
         userService.changePassword(userDetails.getUsername(), passwordDto);
         return ResponseEntity.ok().build();
     }
