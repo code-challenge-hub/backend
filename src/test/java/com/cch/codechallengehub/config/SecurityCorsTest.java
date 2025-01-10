3d9fc641c8e10f2a9622008b4dfca1cef5b996e0
KTG5672 <zzang5992@gmail.com>
2025-01-10 21:47:43 +0900

    feat: 로컬, 개발 서버 CORS 설정

---
+++ src/test/java/com/cch/codechallengehub/config/SecurityCorsTest.java
@@ -0,0 +1,65 @@
+package com.cch.codechallengehub.config;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.cch.codechallengehub.web.dto.user.UserCreateRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SecurityCorsTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void devCorsSuccessTest() throws Exception {
+        UserCreateRequest request = UserCreateRequest.builder()
+            .nickname("test")
+            .email("test@test.com")
+            .password("qwe123!")
+            .role("USER")
+            .build();
+        String body = objectMapper.writeValueAsString(request);
+        mockMvc.perform(post("/api/v1/auth/join")
+                .content(body)
+                .contentType(APPLICATION_JSON)
+                .header("Origin", "http://localhost:5000")
+                .header("Access-Control-Request-Method", "GET"))
+            .andExpect(status().isOk())
+            .andExpect(header().string("Access-Control-Allow-Origin", "http://localhost:5000"))
+            .andDo(print());
+    }
+
+    @Test
+    void devCorsFailTest() throws Exception {
+        UserCreateRequest request = UserCreateRequest.builder()
+            .nickname("test")
+            .email("test@test.com")
+            .password("qwe123!")
+            .role("USER")
+            .build();
+        String body = objectMapper.writeValueAsString(request);
+        mockMvc.perform(post("/api/v1/auth/join")
+                .content(body)
+                .contentType(APPLICATION_JSON)
+                .header("Origin", "http://test.com")
+                .header("Access-Control-Request-Method", "GET"))
+            .andExpect(status().isForbidden())
+            .andExpect(content().string("Invalid CORS request"))
+            .andDo(print());
+    }
+}
