1b6c4e6f22dd4a1c06e63ab99e81a9174bc6d0b7
KTG5672 <zzang5992@gmail.com>
2025-01-21 22:16:16 +0900

    feat: 챌린지 조회 핸들러 작성 및 테스트

--- src/main/java/com/cch/codechallengehub/web/controller/ChallengeController.java
+++ src/main/java/com/cch/codechallengehub/web/controller/ChallengeController.java
@@ -1,13 +1,22 @@
 package com.cch.codechallengehub.web.controller;
 
 import com.cch.codechallengehub.dto.ChallengeCreateDto;
+import com.cch.codechallengehub.dto.ChallengeSearchCondition;
+import com.cch.codechallengehub.dto.ChallengeSearchResult;
+import com.cch.codechallengehub.repository.ChallengeQueryRepository;
 import com.cch.codechallengehub.service.ChallengeService;
 import com.cch.codechallengehub.web.dto.ChallengeCreateRequest;
 import com.cch.codechallengehub.web.dto.ChallengeCreateResponse;
+import com.cch.codechallengehub.web.dto.ChallengeSearchRequest;
+import com.cch.codechallengehub.web.dto.ChallengeSearchResponse;
 import com.cch.codechallengehub.web.mapper.ChallengeCreateMapper;
+import com.cch.codechallengehub.web.mapper.ChallengeSearchMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +29,8 @@ public class ChallengeController {
 
 	private final ChallengeService challengeService;
 	private final ChallengeCreateMapper challengeCreateMapper;
+	private final ChallengeQueryRepository challengeQueryRepository;
+	private final ChallengeSearchMapper challengeSearchMapper;
 
 	@PostMapping("/v1/challenge")
 	public ResponseEntity<?> create(@RequestBody @Valid ChallengeCreateRequest request) {
@@ -27,6 +38,17 @@ public class ChallengeController {
 		Long challengeId = challengeService.createChallenge(challengeCreateDto);
 		ChallengeCreateResponse response = new ChallengeCreateResponse(challengeId);
 		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/v1/challenges")
+	public ResponseEntity<?> getChallenges(@Valid ChallengeSearchRequest request, Pageable pageable) {
+		ChallengeSearchCondition challengeSearchCondition = challengeSearchMapper.requestToCondition(
+			request);
+		Slice<ChallengeSearchResult> slice = challengeQueryRepository.findSlice(
+			challengeSearchCondition, pageable);
+		ChallengeSearchResponse challengeSearchResponse = challengeSearchMapper.sliceToResponse(
+			slice);
+		return ResponseEntity.ok(challengeSearchResponse);
 	}
 
 }
--- src/test/java/com/cch/codechallengehub/web/controller/ChallengeControllerTest.java
+++ src/test/java/com/cch/codechallengehub/web/controller/ChallengeControllerTest.java
@@ -3,27 +3,43 @@ package com.cch.codechallengehub.web.controller;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.cch.codechallengehub.constants.ChallengeLevel;
+import com.cch.codechallengehub.constants.ChallengeStatus;
+import com.cch.codechallengehub.dto.ChallengeSearchResult;
+import com.cch.codechallengehub.dto.ChallengeSearchTechStackResult;
+import com.cch.codechallengehub.repository.ChallengeQueryRepository;
 import com.cch.codechallengehub.service.ChallengeService;
 import com.cch.codechallengehub.web.dto.ChallengeCreateRequest;
 import com.cch.codechallengehub.web.dto.ChallengeCreateResponse;
+import com.cch.codechallengehub.web.dto.ChallengeSearchResponse;
 import com.cch.codechallengehub.web.dto.QuestCreateRequest;
 import com.cch.codechallengehub.web.exception.ErrorCode;
 import com.cch.codechallengehub.web.exception.ErrorResponse;
 import com.cch.codechallengehub.web.mapper.ChallengeCreateMapper;
+import com.cch.codechallengehub.web.mapper.ChallengeSearchMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -33,6 +49,14 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 @WebMvcTest(ChallengeController.class)
 class ChallengeControllerTest {
+
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		public ChallengeSearchMapper challengeSearchMapper() {
+			return Mappers.getMapper(ChallengeSearchMapper.class);
+		}
+	}
 
 	@Autowired
 	WebApplicationContext context;
@@ -44,13 +68,21 @@ class ChallengeControllerTest {
 	@MockBean
 	ChallengeService challengeService;
 
+	@SuppressWarnings("unused")
 	@MockBean
 	ChallengeCreateMapper mapper;
+
+	@MockBean
+	ChallengeQueryRepository challengeQueryRepository;
+
+    @Autowired
+    ChallengeSearchMapper challengeSearchMapper;
 
 	@BeforeEach
 	void setUp() {
 		objectMapper = new ObjectMapper();
 		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 		mockMvc = MockMvcBuilders
 			.webAppContextSetup(context)
 			.addFilters(new CharacterEncodingFilter("UTF-8", true))
@@ -242,4 +274,72 @@ class ChallengeControllerTest {
 			.andDo(print());
 	}
 
+	@Test
+	void challenge_search_api_success() throws Exception {
+		// given
+		List<ChallengeSearchResult> results = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			Long id = (long) i;
+			results.add(
+				ChallengeSearchResult.builder()
+					.challengeId(id)
+					.challengeName("name" + i)
+					.level(ChallengeLevel.BEGINNER)
+					.status(ChallengeStatus.PROCESSING)
+					.techStacks(List.of(
+						new ChallengeSearchTechStackResult(id, "stack" + i, "icon" + i)
+						, new ChallengeSearchTechStackResult(id, "stack" + i, "icon" + i)))
+					. createdDate(LocalDateTime.now())
+					.build()
+			);
+		}
+		PageRequest pageable = PageRequest.of(1, 10);
+		Slice<ChallengeSearchResult> slice = new SliceImpl<>(results, pageable, true);
+		when(challengeQueryRepository.findSlice(any(), any())).thenReturn(slice);
+
+		MockHttpServletRequestBuilder builder = get("/api/v1/challenges")
+			.param("searchLike", "name")
+			.param("status", "PROCESSING")
+			.param("sort", "challengeName,ASC")
+			.param("page", "3")
+			.param("size", "10")
+			.contentType(APPLICATION_JSON);
+
+		// when
+		ResultActions perform = mockMvc.perform(builder);
+
+		// then
+		ChallengeSearchResponse challengeSearchResponse = challengeSearchMapper.sliceToResponse(
+			slice);
+		String resultJson = objectMapper.writeValueAsString(challengeSearchResponse);
+		perform.andExpect(status().isOk())
+			.andExpect(content().string(resultJson))
+			.andDo(print());
+	}
+
+	@Test
+	void challenge_search_api_fail_enum_not_valid() throws Exception {
+		// given
+		MockHttpServletRequestBuilder builder = get("/api/v1/challenges")
+			.param("searchLike", "name")
+			.param("status", "NOT_STATUS")
+			.param("level", "NOT_LEVEL")
+			.param("sort", "challengeName,ASC")
+			.param("page", "3")
+			.param("size", "10")
+			.contentType(APPLICATION_JSON);
+		// when
+		ResultActions perform = mockMvc.perform(builder);
+
+		// then
+		ErrorResponse<ErrorCode> errorResponse = ErrorResponse.<ErrorCode>builder()
+			.errorCode(ErrorCode.NOT_VALID_PARAM)
+			.msg("challenge level is not valid\nchallenge status is not valid")
+			.build();
+		String responseJson = objectMapper.writeValueAsString(errorResponse);
+		perform.andExpect(status().isBadRequest())
+			.andExpect(content().string(responseJson))
+			.andDo(print());
+
+	}
 }
